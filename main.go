2f3d0bf7e2114949e9cf1988585a4a81e711b18d
Karmen <lijiamin@pushplus.com>
2025-04-30 18:04:37 +0800

    1

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof" // 导入 pprof 包，注册 /debug/pprof 端点
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Server started at :8080")
+	fmt.Println("GOCOVERDIR:", os.Getenv("GOCOVERDIR")) // 打印 GOCOVERDIR 环境变量的值
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+
+// http.HandleFunc("/sum", sumHandler)
+func sumHandler(w http.ResponseWriter, r *http.Request) {
+	a, b := 0, 0
+	fmt.Sscanf(r.URL.Query().Get("a"), "%d", &a)
+	fmt.Sscanf(r.URL.Query().Get("b"), "%d", &b)
+	res := a + b
+	w.Write([]byte(fmt.Sprintf("sum=%d", res)))
+}
+
+//$env:GOCOVERDIR="coverdata"
+//go run -cover -coverpkg=./... .
